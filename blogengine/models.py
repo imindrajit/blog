8da0be1560d1ae28b7a6e5b316fb49557adc3ae2
imindrajit <i.sarkar16@gmail.com>
2014-09-15 17:45:58 +0530

    Update models.py

--- blogengine/models.py
+++ blogengine/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils import timezone
+from datetime import datetime
 # Create your models here.
 
 
@@ -8,16 +8,16 @@ class Post(models.Model):
 	author = models.ForeignKey(User)
 	title = models.CharField(max_length=200, unique = True)
 	url = models.SlugField(max_length=40, unique=True)
-	pub_date = models.DateField(default=timezone.now)
+	pub_date = models.DateTimeField(auto_now_add=True)
 	views = models.IntegerField(default=0)
 	body = models.TextField()
-	
+
 	class Meta:
 		ordering = ["-pub_date"]
-	
+
 	def __unicode__(self):
 		return self.title
-	
+
 	def get_absolute_url(self):
 		return "/%s/%s/%s/" % (self.pub_date.year, self.pub_date.month, self.url)
 
